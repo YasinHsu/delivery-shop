bac6a02a1d4167157c1dab5e77a7f0f18693a1f9
YasinHsu <moqaxy@gmail.com>
2024-05-18 17:12:52 +0330

    added new pages

---
+++ lib/map.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+// import 'package:location/location.dart';
+
+class MapScreen extends StatelessWidget {
+  const MapScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FlutterMap(
+        options: const MapOptions(
+          initialCenter: LatLng(36.33704, 59.58296),
+          initialZoom: 13,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          ),
+        ]
+      ),
+     );
+  }
+}
