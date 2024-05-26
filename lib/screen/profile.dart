df3fe005aa7b7b060a95040e3ebbaaa4e7528363
YasinHsu <moqaxy@gmail.com>
2024-05-26 22:52:38 +0330

    commit 2

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -6,226 +6,237 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height * .5,
-                child: Stack(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 40.0),
-                      child: Container(
-                        height: MediaQuery.of(context).size.height,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.elliptical(
-                                MediaQuery.of(context).size.width * 0.5, 100.0),
-                            bottomRight: Radius.elliptical(
-                                MediaQuery.of(context).size.width * 0.5, 100.0),
-                          ),
-                          image: const DecorationImage(
-                            fit: BoxFit.cover,
-                            image: NetworkImage(
-                                'https://i.pinimg.com/564x/1e/7f/85/1e7f85e354e1a11b4a439ac9d9f7e283.jpg'),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: Stack(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 10, top: 10),
-                            child: Icon(
-                              Icons.close,
-                              color: Color(0xffC3C3C3),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 10, top: 10),
-                            child: Align(
-                              alignment: Alignment.topCenter,
-                              child: Text(
-                                'Cora Richardson',
-                                style: TextStyle(
-                                  color: Color(0xffBDBDBD),
-                                  fontSize: 25,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          CircleAvatar(
-                            radius: 30,
-                            backgroundColor: Color(0xffD8D8D8),
-                            child: Icon(
-                              Icons.chat,
-                              size: 30,
-                              color: Color(0xff6E6E6E),
-                            ),
-                          ),
-                          CircleAvatar(
-                            radius: 70,
-                            backgroundImage: NetworkImage(
-                                'https://i.pinimg.com/564x/1e/7f/85/1e7f85e354e1a11b4a439ac9d9f7e283.jpg'),
-                          ),
-                          CircleAvatar(
-                            radius: 30,
-                            backgroundColor: Color(0xffD8D8D8),
-                            child: Icon(
-                              Icons.call,
-                              size: 30,
-                              color: Color(0xff6E6E6E),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+        child: Scaffold(
+      body: Container(
+        color: Colors.white54,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 15,
+            ),
+            const ListTile(
+              leading: Icon(Icons.arrow_back),
+              trailing: Icon(Icons.menu),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                CircleAvatar(
+                  maxRadius: 65,
+                  backgroundImage: AssetImage("assets/6195145.jpg"),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
-                child: Text(
-                  'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters',
-                  style: TextStyle(fontSize: 15),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                CircleAvatar(
+                  backgroundImage: AssetImage("assets/download.png"),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                SizedBox(
+                  width: 15,
+                ),
+                CircleAvatar(
+                  backgroundImage: AssetImage("assets/GooglePlus-logo-red.png"),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                CircleAvatar(
+                  backgroundImage:
+                      AssetImage("assets/1_Twitter-new-icon-mobile-app.jpg"),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                CircleAvatar(
+                  backgroundImage:
+                      AssetImage("assets/600px-LinkedIn_logo_initials.png"),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "Thomas Shelby",
+                  style: TextStyle(fontWeight: FontWeight.w900, fontSize: 26),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [Text("@peakyBlinders")],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "Master manipulator, deal-maker and\n                   entrepreneur",
+                  style: TextStyle(fontSize: 20),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              child: Expanded(
+                  child: ListView(
                 children: [
-                  Text(
-                    'Hacker',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                  Card(
+                    margin:
+                        const EdgeInsets.only(left: 35, right: 35, bottom: 10),
+                    color: Colors.white70,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    child: const ListTile(
+                      leading: Icon(
+                        Icons.privacy_tip_sharp,
+                        color: Colors.black54,
+                      ),
+                      title: Text(
+                        'Privacy',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        color: Colors.black54,
+                      ),
                     ),
                   ),
-                  Text(
-                    '|',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Card(
+                    color: Colors.white70,
+                    margin:
+                        const EdgeInsets.only(left: 35, right: 35, bottom: 10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    child: const ListTile(
+                      leading: Icon(
+                        Icons.history,
+                        color: Colors.black54,
+                      ),
+                      title: Text(
+                        'Purchase History',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        color: Colors.black54,
+                      ),
                     ),
                   ),
-                  Text(
-                    'Dev',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Card(
+                    color: Colors.white70,
+                    margin:
+                        const EdgeInsets.only(left: 35, right: 35, bottom: 10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    child: const ListTile(
+                      leading: Icon(Icons.help_outline, color: Colors.black54),
+                      title: Text(
+                        'Help & Support',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        color: Colors.black54,
+                      ),
                     ),
                   ),
-                  Text(
-                    '|',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Card(
+                    color: Colors.white70,
+                    margin:
+                        const EdgeInsets.only(left: 35, right: 35, bottom: 10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    child: const ListTile(
+                      leading: Icon(
+                        Icons.privacy_tip_sharp,
+                        color: Colors.black54,
+                      ),
+                      title: Text(
+                        'Settings',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Icon(Icons.arrow_forward_ios_outlined),
                     ),
                   ),
-                  Text(
-                    'Android',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Card(
+                    color: Colors.white70,
+                    margin:
+                        const EdgeInsets.only(left: 35, right: 35, bottom: 10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    child: const ListTile(
+                      leading: Icon(
+                        Icons.add_reaction_sharp,
+                        color: Colors.black54,
+                      ),
+                      title: Text(
+                        'Invite a Friend',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        color: Colors.black54,
+                      ),
                     ),
                   ),
-                  Text(
-                    '|',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
+                  const SizedBox(
+                    height: 10,
                   ),
-                  Text(
-                    'Travel',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                  Card(
+                    color: Colors.white70,
+                    margin:
+                        const EdgeInsets.only(left: 35, right: 35, bottom: 10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    child: const ListTile(
+                      leading: Icon(
+                        Icons.logout,
+                        color: Colors.black54,
+                      ),
+                      title: Text(
+                        'Logout',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Icon(Icons.arrow_forward_ios_outlined),
                     ),
-                  ),
+                  )
                 ],
-              ),
-              Container(
-                color: Color(0xffF8F8F8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 16.0),
-                      child: Column(
-                        children: [
-                          Text('Article'),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Text(
-                            '20',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      height: 50,
-                      width: 1,
-                      color: Colors.black,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 16.0),
-                      child: Column(
-                        children: [
-                          Text('Followers'),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Text(
-                            '200',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      height: 50,
-                      width: 1,
-                      color: Colors.black,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 16.0),
-                      child: Column(
-                        children: [
-                          Text('Following'),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Text(
-                            '80',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+              )),
+            )
+          ],
         ),
       ),
-    );
+    ));
   }
 }
