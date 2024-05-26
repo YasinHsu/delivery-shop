87f457a729dee25d217759c4f2f2a10661c736bb
YasinHsu <moqaxy@gmail.com>
2024-05-27 01:32:08 +0330

    little changes

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -15,73 +15,41 @@ class ProfileScreen extends StatelessWidget {
               height: 15,
             ),
             const ListTile(
-              leading: Icon(Icons.arrow_back),
               trailing: Icon(Icons.menu),
             ),
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
+              children: [
                 CircleAvatar(
                   maxRadius: 65,
-                  backgroundImage: AssetImage("assets/6195145.jpg"),
+                  backgroundImage: AssetImage("assets/profile.jpg"),
                 ),
               ],
             ),
             const SizedBox(
               height: 15,
             ),
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                CircleAvatar(
-                  backgroundImage: AssetImage("assets/download.png"),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                CircleAvatar(
-                  backgroundImage: AssetImage("assets/GooglePlus-logo-red.png"),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                CircleAvatar(
-                  backgroundImage:
-                      AssetImage("assets/1_Twitter-new-icon-mobile-app.jpg"),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                CircleAvatar(
-                  backgroundImage:
-                      AssetImage("assets/600px-LinkedIn_logo_initials.png"),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
+              children: [
                 Text(
                   "Thomas Shelby",
                   style: TextStyle(fontWeight: FontWeight.w900, fontSize: 26),
                 )
               ],
             ),
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const [Text("@peakyBlinders")],
+              children: [Text("@peakyBlinders")],
             ),
             const SizedBox(
               height: 15,
             ),
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
+              children: [
                 Text(
-                  "Master manipulator, deal-maker and\n                   entrepreneur",
+                  "about yourself",
                   style: TextStyle(fontSize: 20),
                 )
               ],
@@ -89,151 +57,124 @@ class ProfileScreen extends StatelessWidget {
             const SizedBox(
               height: 15,
             ),
-            Container(
-              child: Expanded(
-                  child: ListView(
-                children: [
-                  Card(
-                    margin:
-                        const EdgeInsets.only(left: 35, right: 35, bottom: 10),
-                    color: Colors.white70,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    child: const ListTile(
-                      leading: Icon(
-                        Icons.privacy_tip_sharp,
-                        color: Colors.black54,
-                      ),
-                      title: Text(
-                        'Privacy',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      trailing: Icon(
-                        Icons.arrow_forward_ios_outlined,
-                        color: Colors.black54,
-                      ),
+            Expanded(
+                child: ListView(
+              children: [
+                Card(
+                  margin:
+                      const EdgeInsets.only(left: 35, right: 35, bottom: 10),
+                  color: Colors.white70,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  child: const ListTile(
+                    leading: Icon(
+                      Icons.privacy_tip_sharp,
+                      color: Colors.black54,
+                    ),
+                    title: Text(
+                      'Privacy',
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      color: Colors.black54,
                     ),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Card(
-                    color: Colors.white70,
-                    margin:
-                        const EdgeInsets.only(left: 35, right: 35, bottom: 10),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    child: const ListTile(
-                      leading: Icon(
-                        Icons.history,
-                        color: Colors.black54,
-                      ),
-                      title: Text(
-                        'Purchase History',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      trailing: Icon(
-                        Icons.arrow_forward_ios_outlined,
-                        color: Colors.black54,
-                      ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Card(
+                  color: Colors.white70,
+                  margin:
+                      const EdgeInsets.only(left: 35, right: 35, bottom: 10),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  child: const ListTile(
+                    leading: Icon(
+                      Icons.history,
+                      color: Colors.black54,
+                    ),
+                    title: Text(
+                      'Purchase History',
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      color: Colors.black54,
                     ),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Card(
-                    color: Colors.white70,
-                    margin:
-                        const EdgeInsets.only(left: 35, right: 35, bottom: 10),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    child: const ListTile(
-                      leading: Icon(Icons.help_outline, color: Colors.black54),
-                      title: Text(
-                        'Help & Support',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      trailing: Icon(
-                        Icons.arrow_forward_ios_outlined,
-                        color: Colors.black54,
-                      ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Card(
+                  color: Colors.white70,
+                  margin:
+                      const EdgeInsets.only(left: 35, right: 35, bottom: 10),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  child: const ListTile(
+                    leading: Icon(Icons.help_outline, color: Colors.black54),
+                    title: Text(
+                      'Help & Support',
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      color: Colors.black54,
                     ),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Card(
-                    color: Colors.white70,
-                    margin:
-                        const EdgeInsets.only(left: 35, right: 35, bottom: 10),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    child: const ListTile(
-                      leading: Icon(
-                        Icons.privacy_tip_sharp,
-                        color: Colors.black54,
-                      ),
-                      title: Text(
-                        'Settings',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      trailing: Icon(Icons.arrow_forward_ios_outlined),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Card(
+                  color: Colors.white70,
+                  margin:
+                      const EdgeInsets.only(left: 35, right: 35, bottom: 10),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  child: const ListTile(
+                    leading: Icon(
+                      Icons.privacy_tip_sharp,
+                      color: Colors.black54,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Card(
-                    color: Colors.white70,
-                    margin:
-                        const EdgeInsets.only(left: 35, right: 35, bottom: 10),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    child: const ListTile(
-                      leading: Icon(
-                        Icons.add_reaction_sharp,
-                        color: Colors.black54,
-                      ),
-                      title: Text(
-                        'Invite a Friend',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      trailing: Icon(
-                        Icons.arrow_forward_ios_outlined,
-                        color: Colors.black54,
-                      ),
+                    title: Text(
+                      'Settings',
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
                     ),
+                    trailing: Icon(Icons.arrow_forward_ios_outlined),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Card(
-                    color: Colors.white70,
-                    margin:
-                        const EdgeInsets.only(left: 35, right: 35, bottom: 10),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    child: const ListTile(
-                      leading: Icon(
-                        Icons.logout,
-                        color: Colors.black54,
-                      ),
-                      title: Text(
-                        'Logout',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      trailing: Icon(Icons.arrow_forward_ios_outlined),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Card(
+                  color: Colors.white70,
+                  margin:
+                      const EdgeInsets.only(left: 35, right: 35, bottom: 10),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  child: const ListTile(
+                    leading: Icon(
+                      Icons.logout,
+                      color: Colors.black54,
                     ),
-                  )
-                ],
-              )),
-            )
+                    title: Text(
+                      'Logout',
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(Icons.arrow_forward_ios_outlined),
+                  ),
+                )
+              ],
+            ))
           ],
         ),
       ),
