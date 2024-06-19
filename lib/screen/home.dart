f9df4e9a96e5266566b47b74462f46b49517ec8f
YasinHsu <moqaxy@gmail.com>
2024-06-19 23:24:26 +0330

    few view changes

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -8,19 +8,42 @@ class HomeScreen extends StatelessWidget {
   final Controller controller = Get.put(Controller());
   HomeScreen({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          children: <Widget>[
+        child: Column(
+          children: [
             _header(),
             _category(),
-            _main(),
             _footer(),
+            _main(),
           ],
         ),
+      ),
+    );
+  }
+
+  Container _header() {
+    return Container(
+      padding: const EdgeInsets.all(1.0),
+      child: Image.asset(
+        'assets/img/header.jpg',
+        height: 80.0,
+      ),
+    );
+  }
+
+  Container _category() {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.black12, borderRadius: BorderRadius.circular(16)),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text('flowers'),
+          Icon(CupertinoIcons.sort_down),
+        ],
       ),
     );
   }
@@ -29,7 +52,7 @@ class HomeScreen extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: SizedBox(
-        height: 300.0,
+        height: 100.0,
         child: CarouselSlider(
           options: CarouselOptions(height: 400.0),
           items: [1, 2, 3, 4, 5].map((i) {
@@ -51,97 +74,78 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  Container _category() {
-    return Container(
-      child: const Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Text('flowers'),
-          Icon(CupertinoIcons.sort_down),
-        ],
-      ),
-    );
-  }
-
-  Container _header() {
-    return Container(
-      padding: const EdgeInsets.all(10.0),
-      child: Image.asset(
-        'assets/img/header.jpg',
-        height: 80.0,
-      ),
-    );
-  }
-
   Expanded _main() {
-  return Expanded(
-    child: Obx(() {
-      if (controller.loading.value) {
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      }
-      if (controller.products.isEmpty) {
-        return const Center(
-          child: Text('check your internet...'),
-        );
-      }
-      return ListView.builder(
-        itemCount: controller.products.length,
-        itemBuilder: (context, index) => Card(
-          elevation: 0.0,
-          child: Container(
-            height: 150,
-            margin: const EdgeInsets.all(8),
-            child: Row(
-              children: [
-                Container(
-                  width: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    image: DecorationImage(
-                        image:
-                            NetworkImage(controller.products[index]["image"]),
-                        fit: BoxFit.contain),
-                  ),
-                  margin: const EdgeInsets.all(4),
-                ),
-                const SizedBox(
-                  width: 4,
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          controller.products[index]["title"],
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        Expanded(
-                          child: Text(
-                            controller.products[index]["description"],
-                            maxLines: 2,
-                            overflow: TextOverflow.fade,
-                          ),
-                        ),
-                        Text(
-                          "\$${controller.products[index]["price"]}",
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ],
+    return Expanded(
+      child: Obx(() {
+          if (controller.loading.value) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (controller.products.isEmpty) {
+            return const Center(
+              child: Text('check your internet...'),
+            );
+          }
+          return ListView.builder(
+            itemCount: controller.products.length,
+            itemBuilder: (context, index) => Card(
+              elevation: 0.0,
+              child: Container(
+                height: 150,
+                margin: const EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                controller.products[index]["image"]),
+                            fit: BoxFit.contain),
+                      ),
+                      margin: const EdgeInsets.all(4),
                     ),
-                  ),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              controller.products[index]["title"],
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Expanded(
+                              child: Text(
+                                controller.products[index]["description"],
+                                maxLines: 2,
+                                overflow: TextOverflow.fade,
+                              ),
+                            ),
+                            Text(
+                              "\$${controller.products[index]["price"]}",
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
-      );
-    },),
-  );
-}
+          );
+        },
+      ),
+    );
+  }
 }
